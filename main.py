96f6e575b9b854fee98cabc572876920e6f4ff37
shallof <ben97shallof@gmail.com>
2023-01-29 12:05:42 +0200

    Update Code

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from doctor import Doctor
 from patient import Patient
 from apppointment import Appointment
+from schedule import Schedule
+from scedule_appintment import Scedule_Appintment
 
 
+
+"""
 doc_1=Doctor("Dr.Ahmed","cardiology")
 pat_1=Patient("Ali",25)
 
 day = input("Please Enter The Day : ")
 app_1=Appointment(doc_1,pat_1,{})
+"""
 
 
--- patient.py
+++ patient.py
@@ -1,4 +1,6 @@
 class Patient :
+    name=''
+    age=0
     def __init__(self,name,age) :
         self.name=name
         self.age=age
